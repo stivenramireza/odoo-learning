f49f08705fa01ef45f4d9bd05039b2f10c209f1f
root <stivenramireza@gmail.com>
2021-04-19 17:30:41 +0000

    Controllers

--- addons/custom_crm/controllers/controllers.py
+++ addons/custom_crm/controllers/controllers.py
@@ -1,14 +1,15 @@
 from odoo import http
+from odoo.http import Response
 import json
 
 
 class VisitController(http.Controller):
 
     @http.route('/api/visits', auth='public', method=['GET'], csrf=False)
-    def get_visits(self, **kwargs):
+    def get_visits(self, **kw):
         try:
             visits = http.request.env['custom_crm.visit'].sudo().search_read([], ['id', 'name', 'customer', 'done'])
             res = json.dumps(visits, ensure_ascii=False).encode('utf-8')
-            return http.Response(res, content_type='application/json;charset=utf-8', status=200)
+            return Response(res, content_type='application/json;charset=utf-8', status=200)
         except Exception as e:
-            return http.Response(json.dumps({'error': e}), content_type='application/json;charset=utf-8', status=505)
+            return Response(json.dumps({'error': e}), content_type='application/json;charset=utf-8', status=505)
